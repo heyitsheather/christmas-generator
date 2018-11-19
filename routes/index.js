4e8bf503dc5e23e05893f4ff166c4c17a497efe1
Heather Richardson <heatherrichardsonphoto@gmail.com>
2018-11-19 19:07:14 +0100

    deleted unnecessary files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const giftRequest = require ("../models/gift-request-model")
+const giftRequest = require ("../models/gift-request-model.js")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
