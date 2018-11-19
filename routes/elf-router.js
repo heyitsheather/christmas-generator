85fb7d3ae5d2263329cdafb4a5fe3d928ff0aff7
filippo fresilli <illiserf@gmail.com>
2018-11-19 19:03:22 +0100

    more css code

--- routes/elf-router.js
+++ routes/elf-router.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router  = express.Router();
 
 
-router.get("/signup", (req, res, next)=> {
+router.get("/become-an-elf-signup", (req, res, next)=> {
   res.render("elf-views/elf-signup.hbs");
+});
+
+router.get("/become-an-elf-login", (req, res, next)=> {
+  // send flash messages to the hbs file as "messages"
+  res.render("elf-views/elf-login.hbs");
 });
 
 module.exports = router;
