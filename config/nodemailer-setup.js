d25a428e0e44dd824953e77d7a25fa49fcb270e8
Heather Richardson <heatherrichardsonphoto@gmail.com>
2018-11-22 16:41:41 +0100

    added send-email.js

---
+++ config/nodemailer-setup.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.gmail_email,
+    pass: gmail_password,
+  },
+});
+
+function sendSignupMail(){
+  return transport.sendMail({
+  from:"Your Secret Helper Elf <santas.workshop@gmail.com>",
+  to:"Mr. Blah <blah@example.com>",
+  subject:"",
+  text:"",
+  html:"",
+});
+}
+
+
+
+function sendSuggestionResultMail(){
+  return transport.sendMail({
+  from:"Your Secret Helper Elf <santas.workshop@gmail.com>",
+  to:"Mr. Blah <blah@example.com>",
+  subject:"",
+  text:"",
+  html:"",
+});
+}
+
+module.exports = { sendSignupMail, sendSuggestionResultMail }
